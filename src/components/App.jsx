23514d5177c4079c049835a64d68bfc6cfb5b16c
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-06-11 20:32:13 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Search } from './Search/Search';
+import { ContentInfo } from './ContentInfo/ContentInfo';
+
+export class App extends Component {
+  state = {
+    searchText: '',
+  };
+
+  handleSearch = searchText => {
+    this.setState({
+      searchText,
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+        
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 16,
+          color: '#010101',
+        }}
+      >
+        <Search handleSearch={this.handleSearch} />
+        <ContentInfo searchText={this.state.searchText} />
+      </div>
+    );
+  }
+}
